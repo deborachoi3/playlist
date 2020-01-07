dff8cf9af00a0756d5256b9d7c7c9dbc677d1a87
deborachoi3 <bbybokchoi@gmail.com>
2020-01-07 02:21:15 +0000

    playlist-deborachoi3

---
+++ js/script.js
@@ -0,0 +1,31 @@
+let songNames = ["Circles"];
+
+songNames.forEach(function(song){
+    $("#songNames").append(song);
+})
+
+let songArtist = ["Post Malone"];
+
+songArtist.forEach(function(artist){
+    $("#songArtist").append(artist);
+})
+
+let songLength= [];
+
+songLength.forEach(function(length){
+    $("#songLength").append(length);
+})
+
+let songImage= [];
+
+songImage.forEach(function(image){
+    $("#songImage").append(image);
+})
+
+let songLink= [];
+
+songLink.forEach(function(link){
+    $("#songLink").append(link);
+})
+
+

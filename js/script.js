8b1efad5136c59567067f35f04430ff73ce994aa
deborachoi3 <bbybokchoi@gmail.com>
2020-02-04 02:23:01 +0000

    Playlist

--- js/script.js
+++ js/script.js
@@ -1,45 +1,69 @@
 let song = {
     songName: "Circles",
     songArtist: "Post Malone",
-    songLength: 336,
+    songLength: 3.36,
     songImage: "https://i.ytimg.com/vi/pQV0WEdT_OE/maxresdefault.jpg",
     songLink: "https://www.youtube.com/watch?v=pQV0WEdT_OE"
 };
 let playlist = [song]
 
+$('.addButton').click(function() {
+  console.log(playlist)  
+    let newSong = {
+        songName: $(".songName").val(),
+        songArtist: $(".songArtist").val(),
+        songLength: $(".songLength").val(),
+        songImage: $(".songImage").val(),
+        songLink: $(".songLink").val()
+    };
 
-$('.addButton').click(function() {
-  playlist.forEach(function(song){
-    $("#songNames").append("<p> name : "+song["songName"]+"</p>");
-    $("#songArtist").append("<p> name : "+song["songArtist"]+"</p>");
-    $("#songLength").append("<p> name : "+song["songLength"]+"</p>");
-    $("#songImage").append("<p> name : "+song["songImage"]+"</p>");
-    $("#songLink").append("<p> name : "+song["songLink"]+"</p>");
-  })
-  
-$('.addButton').click(function() {
-    let songName = $(".songName").val();
-    songNames.push(songName);
+    playlist.append(newSong);
     $("#songNames").empty();
-    songNames.forEach(function(song){
-         $("#songNames").append(song);
-    });
-    console.log(songNames)
+    $("#songArtist").empty();
+    $("#songLength").empty();
+    $("#songImage").empty();
+    $("#songLink").empty();
 
-//     // songArtist.forEach(function(artist){
-//         $("#songArtist").append(artist);
-             
-//     });
+  playlist.forEach(function(song){
+    $("#songNames").append("<p> songName : "+song["songName"]+"</p>");
+    $("#songArtist").append("<p> songArtist : "+song["songArtist"]+"</p>");
+    $("#songLength").append("<p> songLength : "+song["songLength"]+"</p>");
+    $("#songImage").append("<p> songImage : "+song["songImage"]+"</p>");
+    $("#songLink").append("<p> songLink : "+song["songLink"]+"</p>");
+})
 
-//     songLength.forEach(function(length){
-//         $("#songLength").append(length);
-//     });
+    // let songName = $(".songName").val();
+    // songNames.push(songName);
+    // $("#songNames").empty();
+    // songNames.forEach(function(song){
+    //      $("#songNames").append(song);
+    // });
 
-//     songImage.forEach(function(image){
-//         $("#songImage").append(image);
-//     });
+    // let songArtist = $(".songArtist").val();
+    // songArtist.push(songArtist);
+    // $("#songArtist").empty();
+    // songArtist.forEach(function(artist){
+    //      $("#songArtist").append(artist);
+    //  });
 
-//     songLink.forEach(function(link){
-//         $("#songLink").append(link);
-//     });
+    // let songLength = $(".songLength").val();
+    // songLength.push(songLength);
+    // $("#songLength").empty();
+    // songLength.forEach(function(length){
+    //      $("#songLength").append(length);
+    //  });
+
+    // let songImage = $(".songImage").val();
+    // songImage.push(songImage);
+    // $("#songImage").empty();
+    //  songImage.forEach(function(image){
+    //      $("#songImage").append(image);
+    //  });
+
+    // let songLink = $(".songLink").val();
+    // songLink.push(songLink);
+    // $("#songLink").empty();
+    //  songLink.forEach(function(link){
+    //     $("#songLink").append(link);
+    //  });
 });
